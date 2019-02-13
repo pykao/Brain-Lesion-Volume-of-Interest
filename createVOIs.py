23677ce1316a1f06755d628c0b810054ac135316
Po-Yu Kao <poyu_kao@ucsb.edu>
2019-02-12 17:51:15 -0800

    added individual VOI

--- createVOIs.py
+++ createVOIs.py
@@ -7,7 +7,7 @@ def find_percentiles(x):
 	x_float = x_flat.astype(np.float64)
 	y = np.ma.masked_where(x_float == 0, x_float)
 	y = np.ma.filled(y, np.nan)
-	return np.nanpercentile(y, 40), np.nanpercentile(y, 60), np.nanpercentile(y, 80)
+	return np.nanpercentile(y, 25), np.nanpercentile(y, 50), np.nanpercentile(y, 75)
 
 heatmaps_dir = './heatmaps/'
 
@@ -29,9 +29,9 @@ VOI = np.zeros(tumor_nda.shape, tumor_nda.dtype)
 ed25, ed50, ed75 = find_percentiles(edema_nda)
 ne25, ne50, ne75 = find_percentiles(necrosis_nda)
 tu25, tu50, tu75 = find_percentiles(tumor_nda)
-print(ed25, ed50, ed75)
-print(ne25, ne50, ne75)
-print(tu25, tu50, tu75)
+print('Edema:', ed25, ed50, ed75)
+print('Necrosis:', ne25, ne50, ne75)
+print('Tumor:', tu25, tu50, tu75)
 
 VOI[edema_nda>=ed25] = 1
 VOI[necrosis_nda>=ne25] = 2
@@ -47,4 +47,20 @@ VOI[tumor_nda>=tu75] = 9
 
 VOI_img = sitk.GetImageFromArray(VOI)
 VOI_img.CopyInformation(edema_heatmap_img)
-sitk.WriteImage(VOI_img, './VOI.nii.gz')
+
+
+VOI_path = './VOI'
+if not os.path.exists(VOI_path):
+	os.mkdir(VOI_path)
+
+sitk.WriteImage(VOI_img, os.path.join(VOI_path, 'VOI-1mm.nii.gz'))
+
+for i in range(1,10):
+	VOI_lab = np.zeros(VOI.shape, VOI.dtype)
+	VOI_lab_name = 'VOI-1mm-lab'+str(i)+'.nii.gz'
+	VOI_lab[VOI == i] = 1
+	VOI_lab_img = sitk.GetImageFromArray(VOI_lab)
+	VOI_lab_img.CopyInformation(edema_heatmap_img)
+	sitk.WriteImage(VOI_lab_img, os.path.join(VOI_path, VOI_lab_name))
+
+
